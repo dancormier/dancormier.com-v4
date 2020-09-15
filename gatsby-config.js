a357ecdb8e4fce8a1ac1926c5e7fc50d75f7cdf0
Dan Cormier <dancormierall@gmail.com>
2020-09-15 18:56:03 -0400

    update netlify cms asset location

--- gatsby-config.js
+++ gatsby-config.js
@@ -88,7 +88,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/static/assets`,
+        path: `${__dirname}/content/assets`,
         name: 'assets',
       },
     },
