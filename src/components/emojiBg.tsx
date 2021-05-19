70fc1c115f2daf212c57efa8e535b88a45b4ee06
Dan Cormier <dancormierall@gmail.com>
2021-05-19 14:11:39 -0400

    Convert emojiBg component to use Stacks

--- src/components/emojiBg.tsx
+++ src/components/emojiBg.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react'
-import { keyframes } from '@emotion/core'
+// import { keyframes } from '@emotion/core'
 import { Box } from 'theme-ui'
 import { emojiList, useEmojiIndex } from 'components/emoji'
 
-const bgAni = keyframes({
-  '0%': { backgroundPosition: '0% 0%' },
-  '100%': { backgroundPosition: '0% 100%' },
-})
+// const bgAni = keyframes({
+//   '0%': { backgroundPosition: '0% 0%' },
+//   '100%': { backgroundPosition: '0% 100%' },
+// })
 
 function EmojiBG(): React.ReactElement {
   const [emojiIndex] = useEmojiIndex()
@@ -17,19 +17,7 @@ function EmojiBG(): React.ReactElement {
 
   return (
     <Box
-      sx={{
-        animation: `${bgAni} 80s infinite linear`,
-        content: '""',
-        display: ['none', null, 'block'],
-        position: 'fixed',
-        height: '100%',
-        width: '100%',
-        backgroundSize: emojiSize,
-        filter: 'grayscale(0.5)',
-        opacity: 0.03,
-        zIndex: -1,
-        top: 0,
-      }}
+      className="d-block md:d-none ps-fixed h100 t0 w100 z-hide"
       style={{
         backgroundImage: `url("data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 ${
           emojiSize * 0.78
@@ -38,6 +26,10 @@ function EmojiBG(): React.ReactElement {
         }'><foreignObject width='${emojiSize}px' height='${emojiSize}px'><div xmlns='http://www.w3.org/1999/xhtml' style='${emojiStyle}'>${
           emojiList[emojiIndex]
         }</div></foreignObject></svg>")`,
+        backgroundSize: emojiSize,
+        content: '""',
+        filter: 'grayscale(0.5)',
+        opacity: 0.03,
       }}
     />
   )
