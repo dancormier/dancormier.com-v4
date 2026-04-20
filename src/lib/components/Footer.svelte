40b8cc71a7a798f7ec5aaa556e1dbf12baa2eab8
Dan Cormier <dancormierall@gmail.com>
2026-04-20 12:56:24 -0400

    feat: add Footer component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,19 @@
+<footer>
+  <p>© {new Date().getFullYear()} Dan Cormier</p>
+</footer>
+
+<style>
+  footer {
+    font-size: 0.8rem;
+    padding: 1rem 1.5rem;
+    text-align: center;
+  }
+
+  @media (min-width: 832px) {
+    footer {
+      margin: 0 auto;
+      max-width: 900px;
+      text-align: left;
+    }
+  }
+</style>
