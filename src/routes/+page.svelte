8e6d8fb050b2e5686151d54e7a8aa965f1728e0c
Dan Cormier <dancormierall@gmail.com>
2026-04-20 13:06:22 -0400

    feat: add routes — layout, home, 404

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Header from '$lib/components/Header.svelte';
+  import Socials from '$lib/components/Socials.svelte';
+</script>
+
+<Header>
+  <p>I live in North Carolina and work for <a href="https://stackoverflow.com">Stack Overflow</a>.</p>
+  <Socials />
+</Header>
